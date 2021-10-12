096a88efd7b7f9177456d4e68cf08b65a6838695
L00170151 <l00170151@student.lyit.ie>
2021-10-12 11:31:57 -0700

    start repo

---
+++ mvnApp/src/main/java/ie/lyitMvnApps/App.java
@@ -0,0 +1,13 @@
+package ie.lyitMvnApps;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Wooooorld!" );
+    }
+}
